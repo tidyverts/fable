677b9fdd53d97cb53aa93787ec9d134159084791
Unknown <mitch.ohara-wild@monash.edu>
2018-03-28 13:53:32 +1100

    Improved call traversal

--- R/traverse.R
+++ R/traverse.R
@@ -5,11 +5,11 @@ traverse <-  function(x, f = ~ .x, g = ~.x, h = ~.x, base = ~ is_syntactic_liter
   .base <- as_mapper(base)
   
   # base case
-  if(.base(.h(x)))
+  if(.base(x))
     return(.h(x))
   
   # recursive case
-  .f(map(.g(.h(x)), traverse, f=f, g=g, base=base), .h(x))
+  .f(map(.g(x), traverse, f=f, g=g, base=base), .h(x))
 }
 
 traverse_list <- function(x, f = ~ as.list(.x), g = ~.x, h = ~.x, base = ~ !is.list(.x)){
@@ -20,8 +20,8 @@ traverse_list <- function(x, f = ~ as.list(.x), g = ~.x, h = ~.x, base = ~ !is.l
 traverse_call <- function(x,
                           f = as_mapper(~ map(.x, quo_get_expr) %>% as.call %>% new_quosure(env = get_env(.x[[1]]))), # (quo_listcall_to_call)
                           g = as_mapper(~ .x %>% get_expr %>% as.list %>% map(new_quosure, env = get_env(.x))), # (quo_call_to_listcall)
-                          h = enquo,
+                          h = ~ .x,
                           base = ~ !quo_is_call(.x)){
   x <- enquo(x)
-  traverse(!!x, f=f, g=g, h=h, base=base)
+  traverse(x, f=f, g=g, h=h, base=base)
 }
