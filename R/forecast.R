4ca2ed2f9a54822d0b6e6d1b8abf93f21463b5b4
Unknown <mitch.ohara-wild@monash.edu>
2018-04-11 16:41:56 +1000

    Set forecast to be a reexport

--- R/forecast.R
+++ R/forecast.R
@@ -1,18 +1,3 @@
-#' Forecasting time series
-#'
-#' \code{forecast} is a generic function for forecasting from time series or
-#' time series models. The function invokes particular \emph{methods} which
-#' depend on the class of the first argument.
-#'
-#' @param object a time series or time series model for which forecasts are
-#' required
-#' @param h Number of periods for forecasting
-#' @param ... Additional arguments affecting the forecasts produced. 
-#' @return An object of class "\code{forecast}".
-#'
-#' @author Rob J Hyndman
-#' @keywords ts
 #' @importFrom forecast forecast
 #' @export
-# forecast <- function(object, h, ...) UseMethod("forecast")
-
+forecast::forecast
