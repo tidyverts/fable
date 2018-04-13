49c2b0c1424d422bcc53668ef847031a884901dc
Unknown <mitch.ohara-wild@monash.edu>
2018-04-13 18:07:35 +1000

    Updated dataset to reflect tsibble changes

--- data-raw/elecdemand.R
+++ data-raw/elecdemand.R
@@ -1,4 +1,4 @@
 elecdemand <- fpp2::elecdemand
 class(elecdemand) <- c("mts", "ts")
-elecdemand <- as_tsibble(elecdemand, gather = FALSE)
-use_data(elecdemand, overwrite=TRUE)
+elecdemand <- tsibble::as_tsibble(elecdemand, gather = FALSE)
+usethis::use_data(elecdemand, overwrite=TRUE)
