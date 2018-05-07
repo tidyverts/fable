1c045ed6df9aba5ac7c429e416973d06df0f4845
Unknown <mitch.ohara-wild@monash.edu>
2018-05-07 17:10:33 +1000

    key -> key_vars

--- R/multivariate.R
+++ R/multivariate.R
@@ -1,7 +1,7 @@
 #' @importFrom purrr map_dfr
 multi_univariate <- function(data, cl){
   data %>%
-    split_by(!!!key(.)) %>%
+    split_by(key_vars(.)) %>%
     map_dfr(function(x){
       # Re-evaluate cl in environment with split data
       eval_tidy(
