2b7bf1e7097eaee2a831c5830cbf9bb454645ea4
Unknown <mitch.ohara-wild@monash.edu>
2018-05-08 10:25:52 +1000

    Updated special_parser to support required specials

--- R/parse.R
+++ R/parse.R
@@ -1,41 +1,41 @@
 parse_specials <- function(call = NULL, specials = NULL, xreg = TRUE){
-  if(is.null(call)){ # No model specified
-    return(list())
+  if(!is.null(call)){ # No model specified
+    call <- enexpr(call)
+    parsed <- traverse_call(!!call,
+                            g = ~ .x %>%
+                              get_expr %>% # Extract call
+                              as.list %>% # Split call components
+                              .[-1] %>% # Drop function operator (as it is known to be "+")
+                              map(expr), # Rebuild quosure for recursive map
+                            h = function(x){ # Base types
+                              x <- get_expr(x)
+                              if(!is_call(x) || !(call_name(x) %in% names(specials))){
+                                if(!xreg) stop("Exogenous regressors are not supported for this model type")
+                                list(xreg = list(x))
+                              }
+                              else{# Current call is a special function
+                                list(list(x)) %>% set_names(call_name(x))
+                              }
+                            },
+                            f = function(.x, ...) {
+                              merge_named_list(.x[[1]], .x[[2]])},
+                            base = ~ .x %>% get_expr %>% {!is_call(.) || call_name(.) != "+"}
+    )
+  } else {
+    parsed <- list()
   }
-  call <- enexpr(call)
-  parsed <- traverse_call(!!call,
-                g = ~ .x %>%
-                      get_expr %>% # Extract call
-                      as.list %>% # Split call components
-                      .[-1] %>% # Drop function operator (as it is known to be "+")
-                      map(expr), # Rebuild quosure for recursive map
-                h = function(x){ # Base types
-                  x <- get_expr(x)
-                  if(!is_call(x) || !(call_name(x) %in% specials)){
-                    if(!xreg) stop("Exogenous regressors are not supported for this model type")
-                    list(xreg = list(x))
-                  }
-                  else{# Current call is a special function
-                    list(list(x)) %>% set_names(call_name(x))
-                  }
-                },
-                f = function(.x, ...) {
-                  merge_named_list(.x[[1]], .x[[2]])},
-                base = ~ .x %>% get_expr %>% {!is_call(.) || call_name(.) != "+"}
-  )
+  
 
-  # Recursively combine list of exogenous regressors using "+" in-order
-  # if(!is.null(parsed$xreg)){
-  #   parsed$xreg <- list(
-  #     call2("xreg",
-  #           traverse_list(parsed$xreg, 
-  #                         f = ~ call2("+", .x[[1]], .y),
-  #                         g = ~ list(.x[-length(.x)]),
-  #                         h = ~ .x[[length(.x)]],
-  #                         base = ~ length(.x) <= 1)
-  #           )
-  #     )
-  # }
+  # Add required_specials
+  missing_specials <- attr(specials, "required_specials") %>% 
+    .[!(.%in%names(parsed))]
+  parsed <- parsed %>%
+    append(
+      missing_specials %>%
+        map(~ list(call2(.x))) %>%
+        set_names(missing_specials)
+    )
+  
   if(!is.null(parsed$xreg)){
     parsed$xreg <- list(call2("xreg", parsed$xreg))
   }
@@ -98,7 +98,7 @@ parse_model <- function(data, model, specials, univariate = TRUE){
 
 parse_model_rhs <- function(model_rhs, specials){
   model_rhs %>%
-    parse_specials(specials = names(specials)) %>%
+    parse_specials(specials = specials) %>%
     map(~ .x %>%
           map(
             function(special){
