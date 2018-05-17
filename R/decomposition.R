42c54ee0fbc672dca03d3829ab943106bb69d6f6
Unknown <mitch.ohara-wild@monash.edu>
2018-05-17 12:06:08 +1000

    Documented components

--- R/decomposition.R
+++ R/decomposition.R
@@ -6,6 +6,12 @@ modelsplit.dable <- function(object, ...){
   modelsplit(object[["decomp"]], ...)
 }
 
+#' Extract model or decomposition components
+#' 
+#' Extracts the decomposed components from an object, or the states from a state space model.
+#' 
+#' @param object A model or decomposition
+#' 
 #' @export
 components <- function(object, ...){
   UseMethod("components")
