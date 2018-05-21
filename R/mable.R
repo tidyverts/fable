45674ad632d85529a686e86b5074970dfdf2a8d4
Mitchell <mitch.ohara-wild@monash.edu>
2018-05-21 14:33:00 +1000

    Cleaned code spacing and layout for fitted & residuals

--- R/mable.R
+++ R/mable.R
@@ -3,11 +3,11 @@
 residuals.mable <- function(object, ...){
   object %>%
     transmute(!!!key(.),
-      residuals = map2(data, model,
-                       function(data, model) {
-                         data %>% transmute(residuals = data[[expr_text(attr(model, "response"))]] - as.numeric(fitted(model)))
-                       }
-      )
+              residuals = map2(data, model,
+                               function(data, model) {
+                                 data %>% transmute(residuals = data[[expr_text(attr(model, "response"))]] - as.numeric(fitted(model)))
+                               }
+              )
     )%>%
     unnest(.with = id(!!!key(object)))
 }
@@ -17,7 +17,12 @@ residuals.mable <- function(object, ...){
 fitted.mable <- function(object, ...){
   object %>%
     transmute(!!!key(.),
-              fitted = map2(data, model, ~ .x %>% transmute(fitted = as.numeric(fitted(.y))))) %>%
+              fitted = map2(data, model,
+                             function(data, model) {
+                               data %>% transmute(fitted = as.numeric(fitted(model)))
+                             }
+              )
+    ) %>%
     unnest(.with = id(!!!key(object)))
 }
 
