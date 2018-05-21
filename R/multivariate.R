e712ddfcdaeb6734297ba31c25fbed5aede7fa54
Mitchell <mitch.ohara-wild@monash.edu>
2018-05-21 11:28:58 +1000

    Reimplemented multiunivariate to use nest

--- R/multivariate.R
+++ R/multivariate.R
@@ -1,12 +1,19 @@
 #' @importFrom purrr map_dfr
+#' @importFrom dplyr mutate
+#' @importFrom tsibble nest
 multi_univariate <- function(data, cl){
-  data %>%
-    split_by(key_vars(.)) %>%
-    map_dfr(function(x){
-      # Re-evaluate cl in environment with split data
-      eval_tidy(
-        get_expr(cl), 
-        env = child_env(get_env(cl), !!expr_text(get_expr(cl)$data) := x)
-      )
-    })
+  data %>% 
+    group_by(!!!key(data)) %>%
+    nest %>%
+    mutate(model = map(data,
+                       function(x){
+                         # Re-evaluate cl in environment with split data
+                         eval_tidy(
+                           get_expr(cl), 
+                           env = child_env(caller_env(), !!expr_text(get_expr(cl)$data) := x)
+                         )
+                       })
+           ) %>%
+    mutate(data = map(model, ~.x$data[[1]]),
+           model = map(model, ~.x$model[[1]]))
 }
