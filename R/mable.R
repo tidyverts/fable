83ff47dfc90b8d90a717996ff3ad61d7382e65c1
Mitchell <mitch.ohara-wild@monash.edu>
2018-05-21 14:09:12 +1000

    Added fitted and residual methods for mable

---
+++ R/mable.R
@@ -0,0 +1,26 @@
+#' @importFrom stats residuals
+#' @export
+residuals.mable <- function(object, ...){
+  object %>%
+    transmute(!!!key(.),
+      residuals = map2(data, model,
+                       function(data, model) {
+                         data %>% transmute(residuals = data[[expr_text(attr(model, "response"))]] - as.numeric(fitted(model)))
+                       }
+      )
+    )%>%
+    unnest(.with = id(!!!key(object)))
+}
+
+#' @importFrom stats fitted
+#' @export
+fitted.mable <- function(object, ...){
+  object %>%
+    transmute(!!!key(.),
+              fitted = map2(data, model, ~ .x %>% transmute(fitted = as.numeric(fitted(.y))))) %>%
+    unnest(.with = id(!!!key(object)))
+}
+
+key.mable <- function(x){
+  syms(setdiff(colnames(x), c("data", "model")))
+}
