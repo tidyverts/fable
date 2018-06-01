6bc9fa949f33f4bd6ebf606cd8e25723a8458281
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-01 17:40:01 +1000

    Moved data to tsibbledata

--- data-raw/UKLungDeaths.R
+++
@@ -1,2 +0,0 @@
-UKLungDeaths <- tsibble::as_tsibble(cbind(mdeaths, fdeaths), gather = FALSE)
-usethis::use_data(UKLungDeaths, overwrite=TRUE)
--- data-raw/elecdemand.R
+++
@@ -1,4 +0,0 @@
-elecdemand <- fpp2::elecdemand
-class(elecdemand) <- c("mts", "ts")
-elecdemand <- tsibble::as_tsibble(elecdemand, gather = FALSE)
-usethis::use_data(elecdemand, overwrite=TRUE)
