4f12512ade84035c97c31da43762016f96521c72
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-02 17:39:32 +1000

    Changed test to use files relative to installation

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,7 +1,7 @@
 context("test-spelling.R")
 
 test_that("package spell check", {
-  badspell <- spelling::spell_check_package("../../")
+  badspell <- spelling::spell_check_package(file.path(system.file(package="fable"), ".."))
   expect_equal(NROW(badspell), 0, 
                info = capture.output(print(badspell)))
 })
