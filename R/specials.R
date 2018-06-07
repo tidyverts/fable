7c0dbbf1851345714035b61f2c8870e49ed9e4a3
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-07 11:30:12 +1000

    Exported new_specials_env for extensibility

--- R/specials.R
+++ R/specials.R
@@ -1,4 +1,12 @@
-# Create evaluation environment for specials
+#' Create evaluation environment for specials
+#' 
+#' Allows extension packages to make use of the formula parsing of specials.
+#' 
+#' @param ... A named set of functions which used to parse formula inputs
+#' @param parent_env The parent environment of the specials (to find other user objects)
+#' @param required_specials The names of specials which must be provided (and if not, are included used with no inputs).
+#' 
+#' @export
 new_specials_env <- function(..., parent_env = caller_env(), required_specials = NULL){
   child_env(parent_env, !!!dots_list(...)) %>%
     enclass(NULL, required_specials = required_specials)
