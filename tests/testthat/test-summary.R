73cc7ae5c847386eae9516aa412dfa659fe9b98e
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-12 14:10:37 +1000

    Fixed test object name

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -8,7 +8,7 @@ test_that("Summarising mables / fables", {
   fc <- fit %>% 
     forecast()
   
-  fc_sum_default <- fc %>% summary
+  fc_sum <- fc %>% summary
   
   expect_equal(dim(fc_sum), c(24, 4))
   expect_s3_class(fc_sum$`80%`, "hilo")
