3d4bb5cb2b5553e1e696691acfdb594501b07808
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-12 16:56:49 +1000

    Added tests of column classes

--- tests/testthat/test-multivariate.R
+++ tests/testthat/test-multivariate.R
@@ -6,8 +6,9 @@ test_that("multiple univariate", {
     ETS(deaths)
   
   expect_equal(fit$type, c("fdeaths", "mdeaths"))
-  
+  expect_s3_class(fit$model, "lst_mdl")
   fc <- fit %>% forecast
   
   expect_equal(fc$type, c("fdeaths", "mdeaths"))
+  expect_s3_class(fc$forecast, "lst_fc")
 })
