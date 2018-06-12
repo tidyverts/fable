38a100c48c372fddb3905af5636b276d62ccd46d
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-12 14:26:42 +1000

    Added forecast column formatting

--- R/lst_methods.R
+++ R/lst_methods.R
@@ -27,3 +27,38 @@ print.lst_mdl <- function(x, ...){
   print(x)
 }
 
+#' @export
+type_sum.lst_fc <- function(x){
+  "fc"
+}
+
+#' @export
+pillar_shaft.lst_fc <- function(x, ...){
+  pillar::new_pillar_shaft_simple(format(x))
+}
+
+#' @export
+format.lst_fc <- function(x, ...){
+  x %>% map_chr(function(x){
+    dist <- attr(x$distribution, "qname")
+    h <- NROW(x)
+    if(attr(x$distribution, "trans")){
+      dist <- sprintf("t(%s)", dist)
+    }
+    sprintf("~%s [h=%i]", dist, h)
+  })
+}
+
+#' @export
+c.lst_fc <- function(x, ...){
+  enclass(NextMethod(), "lst_fc")
+}
+
+#' @export
+`[.lst_fc` <- c.lst_fc
+
+#' @export
+print.lst_fc <- function(x, ...){
+  class(x) <- "list"
+  print(x)
+}
