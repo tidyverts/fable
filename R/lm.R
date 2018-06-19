d8a38a3dc699b1ab5f27f2f5ad471d74ab226994
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-19 16:25:20 +1000

    Added preliminary LM support

---
+++ R/lm.R
@@ -0,0 +1,52 @@
+#' Fit a linear model with time series components
+#' 
+#' @param data A data frame
+#' @param formula Model specification.
+#' @param ... Additional arguments passed to lm
+#' 
+#' @examples 
+#' 
+#' USAccDeaths %>% LM(log(value) ~ trend() + season())
+LM <- function(data, formula, ...){
+  # Capture user call
+  cl <- call_standardise(match.call())
+  
+  # Coerce data
+  data <- as_tsibble(data)
+  
+  # Handle multivariate inputs
+  if(n_keys(data) > 1){
+    return(multi_univariate(data, cl))
+  }
+  
+  # Define specials
+  specials <- new_specials_env(
+    trend = function(knots = NULL){
+      origin <- min(data[[expr_text(index(data))]])
+      trend(data, knots, origin) %>% as.matrix
+    },
+    season = function(period = "smallest"){
+      season(data, period) %>% as_model_matrix
+    },
+    fourier = function(period = "smallest", K){
+      origin <- min(data[[expr_text(index(data))]])
+      fourier(data, period, K, origin) %>% as.matrix
+    },
+    #xreg is handled by lm
+    parent_env = caller_env()
+  )
+  
+  # Parse model
+  model_inputs <- parse_model(data, formula,
+    specials = new_specials_env(trend = function(...){NULL},
+                                season = function(...){NULL},
+                                fourier = function(...){NULL},
+                                xreg = function(...){NULL})
+  )
+  
+  lm(as.formula(eval_tidy(model_inputs$model), specials), data, ...)
+}
+
+as_model_matrix <- function(tbl){
+  stats::model.matrix(~ ., data = tbl)[,-1]
+}
