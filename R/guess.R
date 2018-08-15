587944197fbb2fac651bdccb54292fcce5d1f570
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-15 15:37:48 +1000

    Improved response guessing function

--- R/guess.R
+++ R/guess.R
@@ -1,10 +1,18 @@
-guess_response <- function(.data, ...){
-  UseMethod("guess_response")
-}
-
-guess_response.tbl_ts <- function(.data, ...){
-  if(length(measured_vars(.data))>1){
+#' @importFrom purrr safely
+guess_response <- function(.data){
+  all_vars <- custom_error(
+    measured_vars,
+    "This model function does not support automatic selection of response variables. Please specify this in the model formula."
+  )(.data)
+  
+  if(length(all_vars)!=1){
     abort("Could not automatically determine the response variable, please provide the response variable in the model specification")
   }
-  sym(measured_vars(.data)[1])
+  
+  out <- sym(all_vars[[1]])
+  inform(sprintf(
+    "Model not specified, defaulting to automatic modelling of the `%s` variable. Override this using the model formula.",
+    expr_text(out)
+  ))
+  out
 }
