02a21557a37bf703da85fe2e55211c42b37c34a8
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-16 19:17:09 +1000

    Removed unused utils

--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,3 @@
-expr_sym <- function(expr){
-  sym(expr_name(expr))
-}
-
-quo_sym <- function(quo){
-  sym(quo_name(quo))
-}
-
-# Small function to combine named lists
-merge_named_list <- function(...){
-  all_names <- dots_list(...) %>% map(names) %>% invoke(c, .) %>% unique
-  all_names %>%
-    map(function(name){
-      dots_list(...) %>% map(function(vals) vals[[name]]) %>% invoke(c, .)
-    }) %>%
-    set_names(all_names)
-}
-
-merge_pos_list <- function(...){
-  all_pos <- dots_list(...) %>% map(seq_along) %>% invoke(c, .) %>% unique
-  all_pos %>%
-    map(function(pos){
-      dots_list(...) %>% map(function(vals) vals[[pos]]) %>% invoke(c, .)
-    }) %>%
-    set_names(names(dots_list(...)[[1]]))
-}
-
 flatten_first_args <- function(parsed_model){
   parsed_model$args <- parsed_model$args %>%
     map(~ if(length(.x) > 1){stop("Only one special of each type is allowed for this model")} else {.x[[1]]}) %>%
@@ -34,45 +7,8 @@ flatten_first_args <- function(parsed_model){
   parsed_model
 }
 
-#' @importFrom purrr imap reduce
-enclass <- function(x, subclass = NULL, ...){
-  dots_list(...) %>%
-    imap(function(value, name) set_names(list(value), name)) %>%
-    reduce(.init = x, # Add attributes (from ...)
-           function(x, attr) {
-             if (!is.null(attr[[1]])) {
-               attr(x, names(attr)) <- attr[[1]]
-             }
-             x
-           }) %>%
-    add_class(subclass)
-}
-
 add_class <- function(x, new_class){
   `class<-`(x, union(new_class, class(x)))
-}
-
-rm_class <- function(x, class){
-  `class<-`(x, class(x)[!(class(x) %in% class)])
-}
-
-exclude <- function(match, vars = tidyselect::peek_vars()){
-  vars[-match(match, vars)]
-}
-
-#' @importFrom purrr safely as_mapper
-custom_error <- function(.f, error){
-  .f <- as_mapper(.f)
-  
-  function(...){
-    result <- safely(.f)(...)
-    if(!is.null(result[["error"]])){
-      abort(error)
-    }
-    else{
-      result[["result"]]
-    }
-  }
 }
 
 #' @importFrom utils tail
