1f172bbc0998993b445ed7ea2e009bbed31ba3bc
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-16 15:31:12 +1000

    Updated tests

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,8 +1,10 @@
 context("test-spelling.R")
 
 test_that("package spell check", {
-  skip_on_travis()
-  badspell <- spelling::spell_check_package("../../")
+  badspell <- spelling::spell_check_package(
+    # Determine package source path by finding README.md
+    dirname(list.files("../../", pattern = "README.md", recursive = TRUE, full.names = TRUE))
+  )
   expect_equal(NROW(badspell), 0, 
                info = capture.output(print(badspell)))
 })
