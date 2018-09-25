893fd17731662633db42890bb63cda0b3058a386
Mitchell <mitch.ohara-wild@monash.edu>
2018-09-25 11:20:12 +1000

    Added generation of possible ets models

--- R/ets2.R
+++ R/ets2.R
@@ -87,11 +87,19 @@ ETS2 <- function(data, formula, ...){
   
   # Rebuild `ets` arguments
   parsed_args <- model_inputs$specials
-  required_args <- parsed_args[c("error", "trend", "season")]
-  required_args %>% map(function(.x){if(length(.x) > 1) {abort("Only one special of each type is allowed for ETS.")}})
+  ets_spec <- parsed_args[c("error", "trend", "season")]
+  ets_spec %>% map(function(.x){if(length(.x) > 1) {abort("Only one special of each type is allowed for ETS.")}})
+  ets_spec <- unlist(ets_spec, recursive = FALSE)
   
   # Get response
   y <- eval_tidy(model_lhs(model_inputs$model), data = data)
+  
+  model_opts <- expand.grid(errortype = ets_spec$error$method,
+                            trendtype = ets_spec$trend$method,
+                            seasontype = ets_spec$season$method,
+                            stringsAsFactors = FALSE)
+  model_opts$damped <- nchar(model_opts$trendtype) > 1
+  model_opts$trendtype <- substr(model_opts$trendtype, 1, 1)
 }
 
 #' @export
