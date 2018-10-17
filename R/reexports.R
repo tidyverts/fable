01d5be77b68f891622bfa7623c7626ffbaed9da0
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-17 15:06:01 +1100

    fablelite is now an import
    Resolves #56

--- R/reexports.R
+++ R/reexports.R
@@ -4,3 +4,36 @@ tsibble::`%>%`
 
 #' @export
 tsibble::as_tsibble
+
+#' @export
+fablelite::accuracy
+
+#' @export
+fablelite::simulate
+
+#' @export
+fablelite::autoplot
+
+#' @export
+fablelite::autolayer
+
+#' @export
+fablelite::components
+
+#' @export
+fablelite::forecast
+
+#' @export
+fablelite::hilo
+
+#' @export
+fablelite::interpolate
+
+#' @export
+fablelite::refit
+
+#' @export
+fablelite::report
+
+#' @export
+fablelite::stream
