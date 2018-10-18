c08285bd6b92846cdd09f2a162f3c0c853bf73ca
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-18 14:16:51 +1100

    Revert making fablelite an import
    This causes more issues with plotting and cannot be changed yet. Opens #56.

--- R/reexports.R
+++ R/reexports.R
@@ -4,36 +4,3 @@ tsibble::`%>%`
 
 #' @export
 tsibble::as_tsibble
-
-#' @export
-fablelite::accuracy
-
-#' @export
-fablelite::simulate
-
-#' @export
-fablelite::autoplot
-
-#' @export
-fablelite::autolayer
-
-#' @export
-fablelite::components
-
-#' @export
-fablelite::forecast
-
-#' @export
-fablelite::hilo
-
-#' @export
-fablelite::interpolate
-
-#' @export
-fablelite::refit
-
-#' @export
-fablelite::report
-
-#' @export
-fablelite::stream
