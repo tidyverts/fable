8d47c0286d2ea825f6ff20b6ae50b63e710ad215
Mitchell <mitch.ohara-wild@monash.edu>
2019-01-04 17:18:33 +1100

    Removed unused utils

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,3 @@
-flatten_first_args <- function(specials){
-  specials %>%
-    map(function(.x) if(length(.x) > 1){stop("Only one special of each type is allowed for this model")} else {.x[[1]]}) %>%
-    set_names(NULL) %>%
-    unlist(recursive = FALSE) %>%
-    as.list # If no args are provided, unlist removes list structure
-}
-
-enclass <- function(x, subclass = NULL, ...){
-  dots_list(...) %>%
-    imap(function(value, name) set_names(list(value), name)) %>%
-    reduce(.init = x, # Add attributes (from ...)
-           function(x, attr) {
-             if (!is.null(attr[[1]])) {
-               attr(x, names(attr)) <- attr[[1]]
-             }
-             x
-           }) %>%
-    add_class(subclass)
-}
-
-add_class <- function(x, new_class){
-  `class<-`(x, union(new_class, class(x)))
-}
-
-rm_class <- function(x, class){ 
-  `class<-`(x, setdiff(class(x), class))
-} 
-
 #' Is an object constant?
 #'
 #' Returns true if the object's numerical values do not vary.
