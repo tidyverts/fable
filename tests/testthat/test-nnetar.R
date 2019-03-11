28abfb5da8bcadce92e924d11e797e1f97e90ed7
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-11 11:21:01 +1100

    Updated nnetar tests for box_cox rename

--- tests/testthat/test-nnetar.R
+++ tests/testthat/test-nnetar.R
@@ -2,12 +2,12 @@ context("test-nnetar")
 
 test_that("NNETAR", {
   airmiles <- as_tsibble(airmiles)
-  air_fit <- airmiles %>% model(NNETAR(BoxCox(value, 0.15)))
+  air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15)))
   expect_output(print(air_fit), regexp = "NNAR\\(1,1\\)")
   
   # Test with xreg
-  air_fit <- airmiles %>% model(NNETAR(BoxCox(value, 0.15) ~ trend()))
-  air_fit <- airmiles %>% model(NNETAR(BoxCox(value, 0.15) ~ trend() + rnorm(length(index))))
+  air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15) ~ trend()))
+  air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15) ~ trend() + rnorm(length(index))))
   
   # Test simulations
   air_fit %>% 
