0630a6927943551ab532c335add7792ac7898872
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-19 15:07:08 +1100

    Reexported fablelite verbs
    Related: #56

--- R/reexports.R
+++ R/reexports.R
@@ -4,3 +4,30 @@ tsibble::`%>%`
 
 #' @export
 tsibble::as_tsibble
+
+#' @export
+fablelite::forecast
+
+#' @export
+fablelite::model
+
+#' @export
+fablelite::imitate
+
+#' @export
+fablelite::accuracy
+
+#' @export
+fablelite::box_cox
+
+#' @export
+fablelite::interpolate
+
+#' @export
+fablelite::stream
+
+#' @export
+fablelite::refit
+
+#' @export
+fablelite::report
