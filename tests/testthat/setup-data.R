872d87c5279f0536e10e390923dd7207fe692f91
Mitchell <mitch.ohara-wild@monash.edu>
2019-04-23 13:12:43 +1000

    Fixed as_tsibble.ts usage

--- tests/testthat/setup-data.R
+++ tests/testthat/setup-data.R
@@ -1,4 +1,4 @@
 context("setup-data.R")
 
 USAccDeaths_tbl <- as_tsibble(USAccDeaths)
-UKLungDeaths <- as_tsibble(cbind(mdeaths, fdeaths), gather = FALSE)
+UKLungDeaths <- as_tsibble(cbind(mdeaths, fdeaths), pivot_longer = FALSE)
