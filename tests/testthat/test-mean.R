7b58c1b4a09e43430991b954c5638f787dd8b265
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-21 16:57:38 +1000

    Added tests for MEAN()
    Related #136

--- tests/testthat/test-mean.R
+++ tests/testthat/test-mean.R
@@ -1,9 +1,29 @@
 context("test-mean")
 
 test_that("MEAN", {
-  fable_fit <- USAccDeaths_tbl %>% model(mean = MEAN(value))
+  fit <- USAccDeaths_tbl %>% model(mean = MEAN(value))
   expect_identical(
-    fitted(fable_fit) %>% select(index, .fitted),
+    fitted(fit) %>% select(index, .fitted),
     USAccDeaths_tbl %>% transmute(.fitted = mean(value))
+  )
+  
+  expect_identical(
+    glance(fit)$sigma, sd(scale(USAccDeaths_tbl$value, scale = FALSE))
+  )
+  
+  expect_identical(
+    residuals(fit)[[".resid"]], USAccDeaths_tbl$value - mean(USAccDeaths_tbl$value)
+  )
+  
+  expect_identical(
+    tidy(fit)$estimate, mean(USAccDeaths_tbl$value)
+  )
+  
+  expect_output(report(fit), "Mean: 8788\\.7917")
+  
+  fc <- fit %>% forecast(h = 3)
+  
+  expect_identical(
+    fc$value, rep(mean(USAccDeaths_tbl$value), 3)
   )
 })
