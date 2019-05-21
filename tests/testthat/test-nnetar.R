7da6c5716f5382b8a2b8868a10293f07b1c39b1c
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-21 23:12:18 +1000

    More extensive tests for nnetar
    Related: #136

--- tests/testthat/test-nnetar.R
+++ tests/testthat/test-nnetar.R
@@ -1,19 +1,79 @@
 context("test-nnetar")
 
-test_that("NNETAR", {
-  airmiles <- as_tsibble(airmiles)
+airmiles <- as_tsibble(airmiles)
+test_that("Automatic NNETAR selection", {
   air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15)))
   expect_output(print(air_fit), regexp = "NNAR\\(1,1\\)")
   
   # Test with xreg
-  air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15) ~ trend()))
   air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15) ~ trend() + rnorm(length(index))))
+  air_fit <- airmiles %>% model(NNETAR(box_cox(value, 0.15) ~ trend()))
   
   # Test simulations
   air_fit %>% 
-    imitate(h=10, times = 100)
+    imitate(h=10, times = 5)
   
   # Test forecasts
-  air_fit %>% 
-    forecast(h=10, times = 100)
+  fc_sim <- air_fit %>% 
+    forecast(h=10, times = 5)
+  fc_boot <- air_fit %>% 
+    forecast(h=10, times = 5, bootstrap = TRUE)
+  expect_equal(
+    fc_sim$value,
+    fc_boot$value,
+    tolerance = 100
+  )
+  
+  # Short series
+  expect_output(
+    UKLungDeaths[1:24,] %>% 
+      model(NNETAR(mdeaths)) %>% 
+      report(),
+    "NNAR\\(4,1,3\\)\\[12\\]"
+  )
+})
+
+test_that("Manual NNETAR selection", {
+  fit <- UKLungDeaths %>% 
+    model(NNETAR(mdeaths ~ AR(p = 3, P = 2)))
+  expect_output(print(fit), regexp = "NNAR\\(3,2,3\\)\\[12\\]")
+  
+  expect_equal(
+    with(augment(fit), .fitted + .resid)[-(1:24)],
+    UKLungDeaths$mdeaths[-(1:24)]
+  )
+  
+  # Short series
+  expect_warning(
+    airmiles[1:5,] %>% 
+      model(NNETAR(value ~ AR(10))),
+    "Reduced number of lagged inputs due to short series"
+  )
+})
+
+
+test_that("NNETAR with bad inputs", {
+  expect_error(
+    airmiles[1:2,] %>% 
+      model(NNETAR(value)),
+    "Not enough data to fit a model"
+  )
+  
+  expect_error(
+    airmiles %>% 
+      model(NNETAR(resp(rep_along(value, NA)))),
+    "All observations are missing, a model cannot be estimated without data"
+  )
+  
+  expect_warning(
+    airmiles %>% 
+      model(NNETAR(resp(rep_along(value, 1)))),
+    "Constant data, setting `AR(p=1, P=0)`, and `scale_inputs=FALSE` "
+  )
+  
+  expect_warning(
+    airmiles %>% 
+      model(NNETAR(value ~ rep_along(value, 1))),
+    "Constant xreg column, setting `scale_inputs=FALSE`"
+  )
 })
