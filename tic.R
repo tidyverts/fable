46b48d60ad0a587ff61e3ec516dbf84fc88e6096
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-31 00:25:43 +1000

    Try updating tic pkgdown approach

--- tic.R
+++ tic.R
@@ -1,24 +1,29 @@
 do_package_checks()
 
-if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-  
-  get_stage("deploy") %>%
-    add_code_step(
-      pkgbuild::compile_dll(),
-      prepare_call = remotes::install_github("r-lib/pkgbuild")
-    ) %>%
-    add_code_step(
-      pkgdown::build_favicon(),
-      prepare_call = install.packages("magick")
-    ) %>%
-    add_step(step_build_pkgdown(run_dont_run = TRUE)) %>%
-    add_code_step(system('echo "fable.tidyverts.org" > docs/CNAME')) %>%
-    add_step(step_push_deploy(path = "docs/", branch = "gh-pages"))
-}
+# if (Sys.getenv("id_rsa") != "") {
+#   # pkgdown documentation can be built optionally. Other example criteria:
+#   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+#   # - `ci()$is_tag()`: Only for tags, not for branches
+#   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+#   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+#   
+#   
+#   
+#   get_stage("before_deploy") %>%
+#     add_step(step_setup_ssh())
+#   
+#   get_stage("deploy") %>%
+#     add_code_step(
+#       pkgbuild::compile_dll(),
+#       prepare_call = remotes::install_github("r-lib/pkgbuild")
+#     ) %>%
+#     add_code_step(
+#       pkgdown::build_favicon(),
+#       prepare_call = install.packages("magick")
+#     ) %>%
+#     add_step(step_build_pkgdown(run_dont_run = TRUE)) %>%
+#     add_code_step(system('echo "fable.tidyverts.org" > docs/CNAME')) %>%
+#     add_step(step_push_deploy(path = "docs/", branch = "gh-pages"))
+# }
+
+do_pkgdown()
