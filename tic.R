a6cbddd25987d90bf202191e40a588be67c9808d
Mitchell <mitch.ohara-wild@monash.edu>
2019-09-11 12:04:05 +1000

    Added tidyverts dev to tic tests

--- tic.R
+++ tic.R
@@ -2,7 +2,8 @@ do_package_checks()
 
 if (Sys.getenv("DEV_VERSIONS") != "") {
   get_stage("install") %>%
-    add_step(step_install_github(c("r-lib/rlang", "tidyverse/dplyr", "tidyverse/tidyr")))
+    add_step(step_install_github(c("r-lib/rlang", "tidyverse/dplyr", "tidyverse/tidyr", 
+                                   "tidyverts/tsibble", "tidyverts/fabletools", "tidyverts/feasts")))
 }
 
 if (Sys.getenv("BUILD_PKGDOWN") != "" && ci()$get_branch() == "master") {
