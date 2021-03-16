a23a1a0c8eb6d4c844f54af23939686db0e787ca
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-03-16 12:38:27 +1100

    Use fabletools xreg handler for TSLM Ref: https://github.com/tidyverts/fabletools/issues/276

--- R/lm.R
+++ R/lm.R
@@ -80,22 +80,7 @@ train_tslm <- function(.data, specials, ...) {
 
 specials_tslm <- new_specials(
   common_xregs,
-  xreg = function(...) {
-    model_formula <- new_formula(
-      lhs = NULL,
-      rhs = reduce(enexprs(...), function(.x, .y) call2("+", .x, .y))
-    )
-    env <- parent.frame()
-    if (!exists("list", env)) env <- base_env()
-
-    env$lag <- lag # Mask user defined lag to retain history when forecasting
-    xreg <- model.frame(model_formula, data = env, na.action = stats::na.pass)
-    mm <- model.matrix(terms(xreg), xreg)
-    if (NROW(mm) == 0 && identical(colnames(mm), "(Intercept)")) {
-      return(matrix(data = 1, nrow = NROW(self$data), dimnames = list(NULL, "(Intercept)")))
-    }
-    mm
-  },
+  xreg = special_xreg(),
   .required_specials = "xreg",
   .xreg_specials = names(common_xregs),
 )
