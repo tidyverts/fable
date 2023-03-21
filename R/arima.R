a3824a32d247b04e862b93bc3b846ae626f72bbd
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-03-22 02:24:38 +1100

    Update docs

--- R/arima.R
+++ R/arima.R
@@ -772,7 +772,7 @@ report.ARIMA <- function(object, ...) {
 #'
 #' Produces forecasts from a trained model.
 #'
-#' @inheritParams fabletools::forecast
+#' @inheritParams generics::forecast
 #' @param specials (passed by [`fabletools::forecast.mdl_df()`]).
 #' @param bootstrap If `TRUE`, then forecast distributions are computed using simulation with resampled errors.
 #' @param times The number of sample paths to use in estimating the forecast distribution when `bootstrap = TRUE`.
